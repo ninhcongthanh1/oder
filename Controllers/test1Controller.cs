9c8232415172d8cdf7262b398c32d6240427084f
ninhcongthanh1 <ninhcongthanh628@gmail.com>
2026-02-07 15:23:02 +0700

    Add files via upload

---
+++ Controllers/test1Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace test12.Controllers
+{
+    public class test1Controller : Controller
+    {
+        public string Index()
+        {
+            return "this is default action.";
+        }
+        public string Home()
+        {
+            return "this is the Home action method.";
+        }
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
